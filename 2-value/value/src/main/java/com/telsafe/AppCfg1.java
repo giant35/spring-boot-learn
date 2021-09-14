0c758c48f9ff4accdfbe330a60bff024145582ec
tangfh <tangfh@telsafe.net.cn>
2021-09-14 15:55:58 +0800

    fix: detail

--- 2-value/value/src/main/java/com/telsafe/AppCfg1.java
+++ 2-value/value/src/main/java/com/telsafe/AppCfg1.java
@@ -3,6 +3,8 @@ package com.telsafe;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * @author tangfh
  * @date 2021/9/14
@@ -11,16 +13,16 @@ import org.springframework.stereotype.Component;
 public class AppCfg1 {
     @Value("${app.key}")
     String key;
-    @Value("${app.urls[0]}")
-    String url0;
-    //NOT WORK @Value("${app.urls}")
-    //NOT WORK List<String> urls;
+//    @Value("${app.urls[0]}")
+//    String url0;
+    @Value("${app.urls}")
+    List<String> urls;
     //NOT WORK @Value("${app.misc}")
     //NOT WORK Map<String, Object> misc;
 
-//    public List<String> getUrls() {
-//        return urls;
-//    }
+    public List<String> getUrls() {
+        return urls;
+    }
 //
 //    public Map<String, Object> getMisc() {
 //        return misc;
@@ -30,7 +32,7 @@ public class AppCfg1 {
         return key;
     }
 
-    public String getUrl0() {
-        return url0;
-    }
+//    public String getUrl0() {
+//        return url0;
+//    }
 }
